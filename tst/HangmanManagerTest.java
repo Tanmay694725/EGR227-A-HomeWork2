37ec3ad173240eb3355b4b016d97d593ffcb7d49
Tanmay Arora <90361139+Tanmay694725@users.noreply.github.com>
2022-03-08 22:05:15 -0800

    Final

--- tst/HangmanManagerTest.java
+++ tst/HangmanManagerTest.java
@@ -1,8 +1,8 @@
 import org.junit.*;     // JUnit tools
 
 import java.util.*;     // Collections
-import java.io.*;       // File access
-import test
+import java.io.*;// File access
+
 
 /**
  * Modify this comment
@@ -14,11 +14,11 @@ public class HangmanManagerTest {
         try {
             Scanner fileScanner = new Scanner(new File(fileName));
             Set<String> dictionary = new HashSet<>();
-            while(fileScanner.hasNext()) {
+            while (fileScanner.hasNext()) {
                 dictionary.add(fileScanner.next());
             }
             return dictionary;
-        } catch(FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Assert.fail("Something went wrong.");      //Something went wrong
         }
         /* Should never be reached. */
@@ -29,9 +29,18 @@ public class HangmanManagerTest {
      * Add comments
      */
     @Test
-    public void test1(){
-        //Implement your test code
+    public void constructorTest() {
+        Set<String> d = txtGetter();
+        constructorHappyCase(d, 8, 3);
+        constructorHappyCase(d, 6, 4000);
+        constructorHappyCase(d, 12, 1200);
+        constructorHappyCase(d, 1, 0);
+        constructorFail(null, 10, 10);
+        constructorFail(null, 10, 10);
+        constructorFail(d, -10, 4);
+        constructorFail(d, 3, -16);
+        constructorFail(d, -1, 8);
+        constructorFail(d, -8, -10);
+        constructorFail(null, 0, 0);
     }
-
-    //Add more tests here
 }
